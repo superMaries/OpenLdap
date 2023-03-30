6c854ff29fc9b77b7aa870d166a6d9fde0075ea3
superMarie <775124775@qq.com>
2023-03-30 18:07:23 +0800

    2023-3-30

---
+++ LDAP/src/test/java/cn/ldap/ldap/LdapApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ldap.ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
