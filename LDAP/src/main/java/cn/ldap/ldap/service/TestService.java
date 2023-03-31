6a362a50d9f0190ecd9b8a611cab7a9a95e2f024
superMarie <775124775@qq.com>
2023-03-31 09:40:34 +0800

    feat(ldap)：controller、service、mapper、entity等初始化包提交

---
+++ LDAP/src/main/java/cn/ldap/ldap/service/TestService.java
@@ -0,0 +1,10 @@
+package cn.ldap.ldap.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestService {
+    public static void main(String[] args) {
+       log.info("sonarlint");
+    }
+}
