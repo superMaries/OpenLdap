ad9e78b67845cb1379390edc129013601cce1579
suntao <775124775@qq.com>
2023-06-05 15:39:17 +0800

    feat(ldap)：从服务查询修改，配置修改

---
+++ LDAP/src/main/java/cn/ldap/ldap/common/dto/RefreshIndexDto.java
@@ -0,0 +1,15 @@
+package cn.ldap.ldap.common.dto;
+
+import lombok.Data;
+
+/**
+ * @author suntao
+ * @create 2023/6/5
+ */
+@Data
+public class RefreshIndexDto {
+
+    private Integer id;
+
+    private String index;
+}
